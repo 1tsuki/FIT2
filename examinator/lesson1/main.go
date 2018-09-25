ca1c266fd36e82e8335047f9a80f6aadccc09b94
Itsuki Sakitsu <itsuki.sakitsu@gmail.com>
2018-09-25 23:06:40 +0900

    implemented lesson1 examinator

--- examinator/lesson1/main.go
+++ examinator/lesson1/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/1tsuki/FIT2/examinator"
 	"github.com/pkg/errors"
 	"io"
 	"net/http"
@@ -10,15 +11,13 @@ import (
 	"os"
 	"path/filepath"
 	"time"
-
-	"github.com/1tsuki/pget"
-	"github.com/1tsuki/FIT2/examinator"
 )
 
 var writer io.Writer
 
 const (
 	exitCodeOK = iota
+	exitCodeInvalidOption
 	exitCodeError
 )
 
@@ -35,132 +34,97 @@ func run(strArgs []string) int {
 	)
 	flags := flag.NewFlagSet("lesson1", flag.ContinueOnError)
 	flags.IntVar(&parallel, "p", 10, "number of download pipelines")
-	flags.StringVar(&list, "s", "./.students", "list of students login ids")
+	flags.StringVar(&list, "s", "../.students", "list of students login ids")
 	flags.Parse(strArgs)
 
-	ex, err := examinator.NewExaminator(list)
-	students, err := readLines(list)
+	ex, err := examinator.NewExaminator(parallel, 30 * time.Second, list)
 	if err != nil {
-		printf("error parsing students list")
+		printf("error parsing student list: %v", err)
+		return exitCodeInvalidOption
+	}
+
+	if err := ex.Download("ex1-1.html", checkHTML); err != nil {
+		printf("error downloading ex1-1.html: %v", err)
 		return exitCodeError
 	}
 
-	ex1HtmlURLs := make([]*url.URL, len(students))
-	ex2HtmlURLs := make([]*url.URL, len(students))
-	ex1CssURLs := make([]*url.URL, len(students))
-	ex2CssURLs := make([]*url.URL, len(students))
-	for key, student := range students {
-		ex1HtmlURL, err := url.Parse(fmt.Sprintf("http://web.sfc.keio.ac.jp/~%s/ex1-1.html", student))
-		if err != nil {
-			printf("error parsing html url: %v", err)
-			return exitCodeError
-		}
-		ex1HtmlURLs[key] = ex1HtmlURL
-
-		ex2HtmlURL, err := url.Parse(fmt.Sprintf("http://web.sfc.keio.ac.jp/~%s/ex1-2.html", student))
-		if err != nil {
-			printf("error parsing html url: %v", err)
-			return exitCodeError
-		}
-		ex2HtmlURLs[key] = ex2HtmlURL
-
-
-		ex1CssUrl, err := url.Parse(fmt.Sprintf("http://web.sfc.keio.ac.jp/~%s/ex1-1.css", student))
-		if err != nil {
-			printf("error parsing css url: %v", err)
-			return exitCodeError
-		}
-		ex1CssURLs[key] = ex1CssUrl
-
-
-		ex2CssUrl, err := url.Parse(fmt.Sprintf("http://web.sfc.keio.ac.jp/~%s/ex1-2.css", student))
-		if err != nil {
-			printf("error parsing css url: %v", err)
-			return exitCodeError
-		}
-		ex2CssURLs[key] = ex2CssUrl
-	}
-
-	downloader := pget.NewPget(10, 60 * time.Second)
-	err = downloader.WithCallback(ex1HtmlURLs, checkHTML)
-	if err != nil {
-		printf("error downloading htmls %v", err)
+	if err := ex.Download("ex1-2.html", checkHTML); err != nil {
+		printf("error downloading ex1-2.html: %v", err)
 		return exitCodeError
 	}
 
-	err = downloader.WithCallback(ex2HtmlURLs, checkHTML)
-	if err != nil {
-		printf("error downloading htmls %v", err)
+	if err := ex.Download("ex1-1.css", checkCSS); err != nil {
+		printf("error downloading ex1-1.css: %v", err)
 		return exitCodeError
 	}
 
-	err = downloader.WithCallback(ex1CssURLs, checkCSS)
-	if err != nil {
-		printf("error downloading csss %v", err)
-		return exitCodeError
-	}
-
-	err = downloader.WithCallback(ex2CssURLs, checkCSS)
-	if err != nil {
-		printf("error downloading csss %v", err)
+	if err := ex.Download("ex1-2.css", checkCSS); err != nil {
+		printf("error downloading ex1-2.css: %v", err)
 		return exitCodeError
 	}
 
 	return exitCodeOK
 }
 
-func checkHTML(url *url.URL, body io.Reader) error {
+func checkHTML(url *url.URL, resp *http.Response) error {
+	if resp.StatusCode == http.StatusNotFound {
+		printf("file did not exist: %s\n", url.String())
+		return nil
+	}
+
 	printf("downloaded file %s\n", url.String())
-	loginName := extractLoginName(url)
-	fileName := extractFileName(url)
+
+	loginName := examinator.ExtractLoginId(url)
+	fileName := examinator.ExtractFileName(url)
 	os.Mkdir(loginName, os.FileMode(0777))
 
-	save := filepath.Join(loginName, fileName)
-	printf("%s\n", save)
-
-	err := saveFile(filepath.Join(loginName, fileName), body)
+	err := examinator.SaveFile(filepath.Join(loginName, fileName), resp.Body)
 	if err != nil {
 		return err
 	}
 
-	validatorURL, err := url.Parse(fmt.Sprintf("https://validator.w3.org/nu/?doc=%s", url.EscapedPath()))
+	validatorURL, err := url.Parse(fmt.Sprintf("https://validator.w3.org/nu/?doc=%s", url.String()))
 	if err != nil {
 		return err
 	}
 
-	resp, err := http.Get(validatorURL.String())
+	resp2, err := http.Get(validatorURL.String())
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
+	defer resp2.Body.Close()
 
-	return saveFile(filepath.Join(loginName, "htmlcheck.html"), resp.Body)
+	return examinator.SaveFile(filepath.Join(loginName, fileName + "_htmlcheck.html"), resp2.Body)
 }
 
-func checkCSS(url *url.URL, body io.Reader) error {
+func checkCSS(url *url.URL, resp *http.Response) error {
+	if resp.StatusCode == http.StatusNotFound {
+		printf("file did not exist: %s\n", url.String())
+		return nil
+	}
 	printf("downloaded file %s\n", url.String())
-	loginName := extractLoginName(url)
-	fileName := extractFileName(url)
+	loginName := examinator.ExtractLoginId(url)
+	fileName := examinator.ExtractFileName(url)
 	os.Mkdir(loginName, os.FileMode(777))
 
-	err := saveFile(filepath.Join(loginName, fileName), body)
+	err := examinator.SaveFile(filepath.Join(loginName, fileName), resp.Body)
 	if err != nil {
 		return err
 	}
 
-	validatorURLStr := fmt.Sprintf("https://jigsaw.w3.org/css-validator/validator?uri=%s&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=ja", url.EscapedPath())
+	validatorURLStr := fmt.Sprintf("https://jigsaw.w3.org/css-validator/validator?uri=%s&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=ja", url.String())
 	validatorURL, err := url.Parse(validatorURLStr)
 	if err != nil {
 		return errors.Wrap(err, fmt.Sprintf("error parsing url %s", validatorURLStr))
 	}
 
-	resp, err := http.Get(validatorURL.String())
+	resp2, err := http.Get(validatorURL.String())
 	if err != nil {
 		return errors.Wrap(err, fmt.Sprintf("failed getting url %s", validatorURL.String()))
 	}
-	defer resp.Body.Close()
+	defer resp2.Body.Close()
 
-	return saveFile(filepath.Join(loginName, "csscheck.html"), resp.Body)
+	return examinator.SaveFile(filepath.Join(loginName, fileName + "_csscheck.html"), resp2.Body)
 }
 
 func printf(format string, a ... interface{}) {
