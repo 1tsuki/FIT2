919e7375400cb69ecbfe9f26791866f094d0787e
Itsuki Sakitsu <itsuki.sakitsu@gmail.com>
2018-10-02 09:21:44 +0900

    implemented lesson2

---
+++ 02_Javascript/ex2-3.js
@@ -0,0 +1,26 @@
+var val = 0;
+function reset() {
+    val = 0;
+    alert(val);
+}
+
+function add() {
+    val += 1;
+    alert(val);
+}
+
+function subtract() {
+    val -= 1;
+    alert(val);
+}
+
+
+function multiple() {
+    val *= 2;
+    alert(val);
+}
+
+function divide() {
+    val /= 2;
+    alert(val);
+}
